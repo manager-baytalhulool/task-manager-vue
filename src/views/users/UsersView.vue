6537ea9668001fede52e60a7d6e3a9c04334cadb
Abdul Wasey BH <abdul.wasey@baytalhulool.com>
2026-01-15 17:43:57 +0500

    User.

--- src/views/users/UsersView.vue
+++ src/views/users/UsersView.vue
@@ -162,13 +162,13 @@ onMounted(async () => {
                 <template #cell-name="{ row: user }">
                   {{ user.name }}
                 </template>
-                <template #cell-actions="{ row: task, rowIndex: index }">
+                <template #cell-actions="{ row: user, rowIndex: index }">
                   <div class="d-flex gap-1">
                     <button class="btn btn-info btn-sm">Start</button>
                     <button class="btn btn-warning btn-sm">Stop</button>
                     <button class="btn btn-info btn-sm">Complete</button>
-                    <button @click="handleEditClick(task)" class="btn btn-info btn-sm">Edit</button>
-                    <button class="btn btn-danger btn-sm" @click="handleDeleteClick(task, index)">
+                    <button @click="handleEditClick(user)" class="btn btn-info btn-sm">Edit</button>
+                    <button class="btn btn-danger btn-sm" @click="handleDeleteClick(user, index)">
                       Delete
                     </button>
                   </div>
